2767f159c6cf8b610e5e0bb1c2d73a0fd4f1f6c4
Mehul <manibhandari2011@gmail.com>
2021-11-09 19:29:41 +0530

    aa

--- src/db/models.js
+++ src/db/models.js
@@ -1,3 +1,4 @@
+const { Sequelize } = require('sequelize')
 const sequelize=require('sequelize')
 const db=new sequelize({
     dialect : 'mysql',
@@ -5,3 +6,42 @@ const db=new sequelize({
     username: 'mehhul',
     password: 'mehhpass',
 })
+const colid ={
+    type: Sequelize.DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+}
+const colusername ={
+    type: Sequelize.DataTypes.STRING(15),
+    unique: true,
+    allowNull: false
+}
+const users= db.define('user',{
+    id: colid,
+    username: colusername
+})
+const posts= db.define('post',{
+    id: colid,
+    body: {
+        type: Sequelize.DataTypes.STRING(100),
+        allowNull:false
+    }
+})
+const comments=db.define('comment',{
+    id:colid,
+})
+users.hasMany(posts)
+posts.belongsTo(users)
+
+posts.hasMany(comments)
+comments.belongsTo(posts)
+
+user.hasMany(comments)
+comments.belongsTo(users)
+
+module.exports= {
+    db,
+    posts,
+    users,
+    comments
+}
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app=express()
 // const db=models.db
 const {db} =require('./db/models')
 
-db.sync()
+db.sync({force:true})   //use {force:true} to create the table again
     .then(()=>{
         app.listen(4444, () =>{
             console.log('Server started at https://localhost:4444')
